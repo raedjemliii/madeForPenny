4f28b1594509768471419d724255167267571139
raedjemliii <yakabol1234@gmail.com>
2024-06-27 14:29:43 +0100

    Update password.js

--- password.js
+++ password.js
@@ -23,19 +23,31 @@ document.addEventListener('DOMContentLoaded', () => {
             if (document.getElementById('remember-me').checked) {
                 localStorage.setItem('password', password);
             }
-            window.location.href = './index2.html';
+            window.location.href = 'index2.html';
         } else {
             alert('Incorrect password');
         }
     });
 
     if (localStorage.getItem('password') === 'penny') {
-        window.location.href = './index2.html';
+        window.location.href = 'index2.html';
     }
 });
 
 function togglePasswordVisibility() {
     const passwordInput = document.getElementById('password');
+    const toggleIcon = document.querySelector('.toggle-password');
     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordInput.setAttribute('type', type);
+
+    if (type === 'password') {
+        toggleIcon.src = 'See No Evil Monkey Emoji.png';
+    } else {
+        toggleIcon.src = 'Hear No Evil Monkey Emoji.png';
+    }
+
+    toggleIcon.classList.add('animate-icon');
+    setTimeout(() => {
+        toggleIcon.classList.remove('animate-icon');
+    }, 300);
 }
